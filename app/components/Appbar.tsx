529e1b38d850ac3ccb87c2b5e929f1dcc17a5710
David Doria <daviddoria@Davids-MacBook-Pro-2.local>
2024-09-10 17:23:59 -0700

    init commit

---
+++ app/components/Appbar.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { signIn, signOut, useSession } from "next-auth/react";
+
+export function Appbar() {
+  const session = useSession();
+
+  return (
+    <div>
+      <div className="flex justify-between">
+        <div>Muzi</div>
+        <div>
+          {session.data?.user ? (
+            <button className="m-2 p-2 bg-blue-400" onClick={() => signOut()}>
+              SignOut
+            </button>
+          ) : (
+            <button className="m-2 p-2 bg-blue-400" onClick={() => signIn()}>
+              SignIn
+            </button>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
